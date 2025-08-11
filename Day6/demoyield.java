86705fce0c43d1942e664d266b986d151001cdbd
poojas2003 <96231234+poojas2003@users.noreply.github.com>
2025-08-11 06:48:54 +0000

    thead

---
+++ Day6/demoyield.java
@@ -0,0 +1,25 @@
+class one extends Thread
+  {
+     public void run()
+	{
+	  for(var i=0;i<10;i++)
+		{
+	          System.out.println(Thread.currentThread().getName()+"-iteration"+i);
+		 }
+         }
+   }
+ 
+ 
+class demoyield
+{
+    public static void main(String asd[])
+	{
+	   one obj1=new one();
+	   obj1.start();
+	   //obj1.yield();
+	   for(var i=0;i<10;i++)
+		{
+		   System.out.println("Main thread running");
+		}
+	}  
+}

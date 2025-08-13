a49d3dfd945a112ea2870de4d40c5c650c481e46
poojas2003 <96231234+poojas2003@users.noreply.github.com>
2025-08-13 09:44:13 +0000

    Byte array input stream

---
+++ Day8/byteArrayInputStream.java
@@ -0,0 +1,21 @@
+import java.io.ByteArrayInputStream;
+class byteArrayInputStream
+   {
+     public static void main(String asd[])
+	{
+	    byte x[]={65,66,67,68,69,70};
+	    ByteArrayInputStream bis=new ByteArrayInputStream(x);
+	    //System.out.println((char)bis.read());
+	    System.out.println(bis.available());
+ 
+	     int i=0;
+ 
+		while((i=bis.read())!=-1)
+		  {
+   		     System.out.println((char)i);
+		  }
+	 System.out.println(bis.available());
+ 
+ 
+	}
+  } 

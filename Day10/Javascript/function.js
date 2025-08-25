43e09afdd9fcf8bc806ebc50c15c2ab089854970
Pooja  LNU <pooja.lnu@i-exceed.com>
2025-08-25 17:11:10 +0530

    function

---
+++ Day10/Javascript/function.js
@@ -0,0 +1,25 @@
+"use strict";
+ 
+let age=12;
+ 
+if(age<18)
+{
+   
+   var greeting= function ()
+     {
+        console.log("welcome you all and wait for your turn!");
+     };
+ 
+     
+}
+else
+{
+    var greeting=function()
+     {
+        console.log("welcome you all and you are all eligible to write the exam!");
+     };
+     
+}
+ 
+greeting();
+ 

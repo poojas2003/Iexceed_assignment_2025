0e254434d98506332c4112fe3e9b499dbc97be40
Pooja  LNU <pooja.lnu@i-exceed.com>
2025-08-25 17:07:46 +0530

    creation of the objects using the methods

---
+++ Day10/Javascript/creationOfObj.js
@@ -0,0 +1,27 @@
+// creating object by literal
+
+const student={name:"pooja",age:21,address:"Kalburagi",marks:90};
+console.log(student);
+
+// creating object by constructor (object)
+const student1=new Object();
+student1.name="Pooja";
+student1.age=21;                    
+student1.address="Kalburagi";
+student1.marks=90;
+
+console.log(student1);
+
+//we can aslo create empty object and add properties later
+const student3={};
+student3.name="Pooja";
+student3.age=21;
+student3.address="kALBURAGI";
+console.log(student3);
+
+//creation of object using new keyword
+const student4=new Object();
+student4.name="Pooja";
+student4.age=21;
+student4.address="Mysore";
+console.log(student4);
